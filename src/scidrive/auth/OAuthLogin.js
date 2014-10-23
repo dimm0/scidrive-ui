addf7e56a36155251b3d62b2d84282353d4c9797
Dmitry Mishin <dmitry.mishin@gmail.com>
2014-10-22 17:10:12 -0700

    Changed title to SDSC, fixed bug with mixing - was unable to do PUT and POST

--- src/scidrive/auth/OAuthLogin.js
+++ src/scidrive/auth/OAuthLogin.js
@@ -6,7 +6,7 @@ define( [
     "dojo/fx",
     "dojo/aspect",
     "dojo/dom-construct",
-    "dojo/request/xhr", 
+    "dojo/request/xhr",
     "dojo/json",
     "dojo/io-query",
     "dojo/has",
@@ -163,7 +163,7 @@ function(declare, lang, fx, connect, coreFx, aspect, domConstruct, xhr, JSON, io
         }
 
 
-        function failure(data, ioargs) { 
+        function failure(data, ioargs) {
             if(ioargs.xhr.status == 400 || ioargs.xhr.status == 401 || ioargs.xhr.status == 503) { // OAuth errors
                 var errorResponse = ioargs.xhr.responseText;
                 if(errorResponse.split("&")[0] != undefined) {
@@ -258,7 +258,7 @@ function(declare, lang, fx, connect, coreFx, aspect, domConstruct, xhr, JSON, io
                     } else {
                         alert("Error logging in: "+ ioargs.xhr.responseText);
                     }
-                    
+
                 }
             },that.credentials));
         });
@@ -306,7 +306,7 @@ function(declare, lang, fx, connect, coreFx, aspect, domConstruct, xhr, JSON, io
         }
 
         //component._refreshRegions();
-            
+
     },
 
     request: function(url, method, args) {
@@ -318,18 +318,38 @@ function(declare, lang, fx, connect, coreFx, aspect, domConstruct, xhr, JSON, io
         var params = {
             headers: {
                 'Authorization': OAuth.sign(
-                    method, 
-                    {url: url}, 
+                    method,
+                    {url: url},
                     this.credentials)
                 .headers["Authorization"]
             }
         };
 
         if("undefined" !== typeof args)
-            params = lang.mixin(params, args);
+            this.mixinDeep(params, args);
 
-        params = lang.mixin(params, {"method": method});
+        this.mixinDeep(params, {"method": method});
         return params;
+    },
+
+    mixinDeep: function(dest, source) {
+     //Recursively mix the properties of two objects
+     var empty = {};
+     for (var name in source) {
+          if(!(name in dest) || (dest[name] !== source[name] && (!(name in empty) || empty[name] !== source[name]))){
+               try {
+                    if ( source[name].constructor==Object ) {
+                         dest[name] = this.mixinDeep(dest[name], source[name]);
+                    } else {
+                         dest[name] = source[name];
+                    };
+               } catch(e) {
+                    // Property in destination object not set. Create it and set its value.
+                    dest[name] = source[name];
+               };
+          };
+     }
+     return dest;
     }
   });
 });
